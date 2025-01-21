01f5e3a7b5775e88f883993e2a996fe2012c2182
CagriCatik <mccatik@gmail.com>
2025-01-22 00:54:24 +0100

    test deployment

--- webpage/docusaurus.config.js
+++ webpage/docusaurus.config.js
@@ -11,26 +11,15 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Python Guide',
-  tagline: '****',
+  tagline: ' ',
   favicon: 'img/favicon.ico',
+  url: 'https://CagriCatik.github.io',
+  baseUrl: '/Python/',
+  organizationName: 'CagriCatik',
+  deploymentBranch: 'gh-pages',
+  onBrokenLinks: 'ignore',
+  onBrokenMarkdownLinks: 'ignore',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
